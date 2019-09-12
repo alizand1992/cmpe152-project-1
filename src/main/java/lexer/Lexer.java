b52ba6c4f594b082daa14bc6a8ce9e49af14f2bf
Ali Zand <ali.zand@sjsu.edu>
2019-09-12 16:41:03 -0700

    Lexer styff

--- src/main/java/lexer/Lexer.java
+++ src/main/java/lexer/Lexer.java
@@ -1,4 +1,39 @@
 package lexer;
 
+import java.util.LinkedList;
+
 public class Lexer {
+    private LinkedList<Token> tokens;
+    private String line;
+
+    public Lexer() {
+        tokens = new LinkedList<Token>();
+    }
+
+    public Lexer(String str) {
+        tokens = new LinkedList<>();
+        line = str;
+        tokens.add(new Token("abcd", "ID"));
+        tokens.add(new Token(";"));
+    }
+
+//    public Lexer(Lexer rhs) {
+//        tokens = (LinkedList<Token>)rhs.getTokens();
+//    }
+
+    public Token getNextToken() {
+        return null;
+    };
+
+    public LinkedList<Token> getTokens() {
+        return this.tokens;
+    }
+
+    public static boolean isValid(String pattern) {
+        return false;
+    }
+
+    public static boolean isId(String pattern) {
+        return false;
+    }
 }
--- src/test/java/lexer/LexerTest.java
+++ src/test/java/lexer/LexerTest.java
@@ -1,4 +1,28 @@
 package lexer;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LexerTest {
+    @Test
+    public void idWithSemiColonIsRecogonizedAsTwoTokens() {
+        // Example abcd;
+        Lexer lex = new Lexer("abcd;");
+        assertEquals(2, lex.getTokens().size());
+    }
+
+    @Test
+    public void idWithSemiColonHasAnIDAndSemicolonToken() {
+        Lexer lex = new Lexer("abcd;");
+        assertEquals("ID", ((Token)(lex.getTokens().get(0))).getName());
+        assertEquals(";", ((Token)(lex.getTokens().get(1))).getName());
+    }
+
+    @Test
+    public void incorrectTokenReturnsNull() {
+        Lexer lex = new Lexer("9abc");
+
+        assertEquals(null, lex.getNextToken());
+    }
 }
