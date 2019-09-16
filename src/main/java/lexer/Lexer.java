619466abdfebd0acf4a8a0108408c219861796f7
Ali Zand <ali.zand@sjsu.edu>
2019-09-16 13:15:09 -0700

    Add missing test, make test go green, add helper methods

--- src/main/java/lexer/Lexer.java
+++ src/main/java/lexer/Lexer.java
@@ -23,8 +23,39 @@ public class Lexer {
         return this.tokens;
     }
 
+    /**
+     *  This method is meant to decide if a pattern is an ID token or not.
+     *  pattern used is [A-Za-z][A-Za-z0-9_]*
+     *  first character:
+     *    upper or lower case alphabet
+     *  All other characters:
+     *    upper or lower case alphabet
+     *    numbers
+     *    underscore
+     *
+     * @param pattern
+     * @return whether or not the pattern is an ID
+     */
     public static boolean isId(String pattern) {
-        return false;
+        char firstChar = pattern.charAt(0);
+        if (firstChar >= '0' && firstChar <= '9') {
+            return false;
+        }
+
+        if (firstChar == '_') {
+            return false;
+        }
+
+        for (int i = 0; i < pattern.length(); i++) {
+            boolean illegalChar = false;
+            char currentChar = pattern.charAt(i);
+
+            if (!(isLowerCase(currentChar) || isUpperCase(currentChar) || isDigit(currentChar) || currentChar == '_')) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static boolean isNum(String pattern) {
@@ -33,5 +64,37 @@ public class Lexer {
 
     public static boolean isReal(String pattern) {
         return  false;
+    }
+
+
+    // Private helper methods
+    /**
+     * This method takes a character and returns true if it is a lower case alphabet character
+     *
+     * @param c
+     * @return
+     */
+    private static boolean isLowerCase(char c) {
+        return c >= 'a' && c <= 'z';
+    }
+
+    /**
+     * This methid takes a character and returns true if it is an upper case alphabet character
+     *
+     * @param c
+     * @return
+     */
+    private static boolean isUpperCase(char c) {
+        return c >= 'A' && c <= 'Z';
+    }
+
+    /**
+     * This method takes a character and returns true if it is a number character.
+     *
+     * @param c
+     * @return
+     */
+    private static boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
     }
 }
